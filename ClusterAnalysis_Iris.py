0863e93a200494edd3b17bb135426306b686d60d
Khushi <71957748+khushi-sabarad@users.noreply.github.com>
2025-02-14 21:52:51 +0530

    Update ClusterAnalysis_Iris.py

--- ClusterAnalysis_Iris.py
+++ ClusterAnalysis_Iris.py
@@ -1,4 +1,4 @@
-#Cluster Analysis,Iris dataset
+# Cluster Analysis, Iris dataset
 from sklearn.datasets import load_iris
 from sklearn.cluster import AgglomerativeClustering
 import matplotlib.pyplot as plt
@@ -6,19 +6,18 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 
 data = load_iris()
 df = data.data
-df = df[:,:]
-z = linkage(df, method= "ward")
-dendro=dendrogram(z)
+
+z = linkage(df, method="ward")
+dendro = dendrogram(z)
 plt.title('Dendrogram')
 plt.ylabel('Euclidean distance')
 plt.show()
-ac = AgglomerativeClustering(n_clusters=3,affinity="euclidean", linkage="ward")
 
-labels= ac.fit_predict(df)
-plt.figure(figsize = (8,5))
-plt.scatter(df[labels == 0, 0], df[labels == 0,1],c="red")
-plt.scatter(df[labels == 1, 0], df[labels==1, 1],c="blue")
-plt.scatter(df[labels == 2, 0], df[labels== 2, 1],c="green")
-plt.scatter(df[labels == 3, 0], df[labels== 3, 1],c="black")
-plt.scatter(df[labels == 4, 0], df[labels== 4, 1],c="orange")
+ac = AgglomerativeClustering(n_clusters=3, linkage="ward") 
+
+labels = ac.fit_predict(df)
+plt.figure(figsize=(8, 5))
+plt.scatter(df[labels == 0, 0], df[labels == 0, 1], c="red")
+plt.scatter(df[labels == 1, 0], df[labels == 1, 1], c="blue")
+plt.scatter(df[labels == 2, 0], df[labels == 2, 1], c="green")
 plt.show()
